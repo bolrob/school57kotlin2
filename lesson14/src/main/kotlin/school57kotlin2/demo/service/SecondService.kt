e83d729192779b9f69342722d58e3aaff0915198
t.p.maerov <mar156in@gmail.com>
2025-03-22 02:17:59 +0300

    lesson14

--- lesson14/src/main/kotlin/school57kotlin2/demo/service/SecondService.kt
+++ lesson14/src/main/kotlin/school57kotlin2/demo/service/SecondService.kt
@@ -1,22 +1,13 @@
 package school57kotlin2.demo.service
 
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.stereotype.Component
 import java.util.*
 
 
-@Component
+//@Component
 class SecondService(
     val random: Random
 ) {
 
     fun getNextInt() = random.nextInt()
 
-}
-
-@Configuration
-class RandomConfig {
-    @Bean
-    fun random() = Random()
 }
